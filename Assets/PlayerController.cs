e42f0fe09ad871eaf9f2aa8e1976c286ed765b76
Jesse Millar <hellojessemillar@gmail.com>
2019-02-24 18:50:10 -0600

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Walk();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void Walk()
+    {
+        if (rb.velocity.magnitude > 0.5)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity) * Quaternion.Euler(0, 0, Mathf.Sin(Time.time * 10) * 40);
+            transform.position = new Vector3(transform.position.x, 1 + Mathf.PingPong(Time.time * 2, 0.6f), transform.position.z);
+        }
+        else
+        {
+            // Lerp back to standing straight up
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0), Time.deltaTime * 10);
+            transform.position = new Vector3(transform.position.x, 1, transform.position.z);
+        }
+    }
+}
